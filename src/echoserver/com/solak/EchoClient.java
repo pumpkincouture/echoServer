49aefa33ec926198fce32db390588a5190e2fbbe
Sylwia Olak <pumpkincouture@gmail.com>
2015-03-31 14:54:16 -0500

    Changed out.write in server to out.println; echo back from server working

--- src/echoserver/com/solak/EchoClient.java
+++ src/echoserver/com/solak/EchoClient.java
@@ -7,20 +7,19 @@ public class EchoClient {
     public static void main(String[] args) throws IOException {
 
         if (args.length != 2) {
-            System.err.println(
-                    "Usage: java EchoClient <host name> <port number>");
+            System.err.println("Usage: java EchoClient <host name> <port number>");
             System.exit(1);
         }
 
         String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
 
-        while (true) {
-            Socket echoSocket = new Socket(hostName, portNumber);
-            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        Socket echoSocket = new Socket(hostName, portNumber);
+        PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
+        while (true) {
             String userInput;
             while ((userInput = stdIn.readLine()) != null) {
                 out.println(userInput);
--- src/echoserver/com/solak/EchoServer.java
+++ src/echoserver/com/solak/EchoServer.java
@@ -11,19 +11,17 @@ public class EchoServer {
             System.exit(1);
         }
 
-        while (true) {
-
             int portNumber = Integer.parseInt(args[0]);
-            System.out.println(portNumber);
-            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            ServerSocket serverSocket = new ServerSocket(portNumber);
             Socket clientSocket = serverSocket.accept();
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+        while (true) {
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-                System.out.println(inputLine);
-                out.write(inputLine);
+                System.out.println("INFO:" + inputLine);
+                out.println(inputLine);
 
             }
         }
