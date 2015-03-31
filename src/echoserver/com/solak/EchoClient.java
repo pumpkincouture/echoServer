a14c267adc776dc25c68d4d98a2b087c5e9c2f08
Sylwia Olak <pumpkincouture@gmail.com>
2015-03-31 10:49:50 -0500

    Fixed loop in server, echoing within IDE

---
+++ src/echoserver/com/solak/EchoClient.java
@@ -0,0 +1,30 @@
+package echoserver.com.solak;
+
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            System.err.println(
+                    "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        Socket echoSocket = new Socket(hostName, portNumber);
+        PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+
+    }
+}
---
+++ src/echoserver/com/solak/EchoServer.java
@@ -0,0 +1,28 @@
+package echoserver.com.solak;
+
+import java.net.*;
+import java.io.*;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+        System.out.println(portNumber);
+        ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+                out.write(inputLine);
+
+            }
+    }
+}
