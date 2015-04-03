71b5fd1861f67fb91862e7490093d371ec064896
Sylwia Olak <pumpkincouture@gmail.com>
2015-04-03 09:44:32 -0500

    Adding post statements, trying to send image via bytes.

--- src/echoserver/com/solak/EchoServer.java
+++ src/echoserver/com/solak/EchoServer.java
@@ -2,33 +2,125 @@ package echoserver.com.solak;
 
 import java.net.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
 
-            ServerSocket serverSocket = new ServerSocket(2345);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
 
-            FileInputStream file = new FileInputStream("./public/testfile.txt");
-            BufferedReader textReader = new BufferedReader(new InputStreamReader(file));
+            while (true) {
 
-            String line = null;
 
-            while ((line = textReader.readLine()) != null) {
-                System.out.println(line);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                FileInputStream file = new FileInputStream("./public/testfile.txt");
+                BufferedReader textReader = new BufferedReader(new InputStreamReader(file));
+
+                OutputStream outStream = clientSocket.getOutputStream();
+
+                List<Hashtable<String, String>> storedData = new ArrayList<Hashtable<String, String>>();
+
+                String line = null;
+
+                while ((line = textReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                textReader.close();
+
+
+                String inputLine = in.readLine();
+//                System.out.println("INFO: " + inputLine);
+//                out.println(inputLine);
+
+                if (inputLine.contains("GET /")) {
+                    out.flush();
+                    out.write("HTTP/1.1 200 OK");
+                    out.write("Content-Type: text/plain");
+                    out.write("Date: " + new Date());
+                    out.write("\r\n");
+                    out.flush();
+
+                } else if (inputLine.contains("GET /form")) {
+                    out.flush();
+                    out.write("HTTP/1.1 200 OK");
+                    out.write("Content-Type: text/plain");
+                    out.write("Date: " + new Date());
+                    out.write("\r\n");
+                    out.write(formResponse());
+                    out.flush();
+                }
+
+                else if (inputLine.contains("POST /form")) {
+                    Hashtable<String, String> data = new Hashtable<>();
+
+                    save(storedData, data);
+                    out.flush();
+                    out.write("HTTP/1.1 200 OK");
+                    out.write("");
+                    out.flush();
+                    System.out.println(data);
+                }
+                else if (inputLine.contains("PUT /form")) {
+                    out.flush();
+                    out.write("HTTP/1.1 200 OK");
+                    out.flush();
+                }
+                else if (inputLine.contains("GET /flower.png")) {
+                    byte[] bytes = convertToByteArray(".public/flower.png");
+
+                    out.flush();
+                    outStream.write(bytes);
+                    out.flush();
+                }
+
+                else {
+                    out.flush();
+                    System.out.println("In the else");
+                    out.write("HTTP/1.1 404 Not Found");
+                    out.flush();
+                }
+
+                System.out.println("before the close statement");
+                System.out.println(formResponse());
+                in.close();
+
             }
 
-            textReader.close();
-
-        while (true) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("INFO: " + inputLine);
-                out.println(inputLine);
-
-            }
+        } catch (Exception err) {
+            System.out.println(err);
+            err.printStackTrace();
         }
+    }
+
+    public static byte[] convertToByteArray(String file) throws IOException {
+        Path path = Paths.get(file);
+        byte[] data = Files.readAllBytes(path);
+
+        return data;
+    }
+
+    private static void save(List<Hashtable<String, String>> savedData, Hashtable data) {
+        savedData.add(data);
+    }
+
+    private static String formResponse() {
+        String responseString = "<form action=\"http://127.0.0.1:5000\" enctype=\"multipart/form-data\"" +
+                "method=\"post\">" +
+                "Enter the name of the File <input name=\"file\" type=\"file\"><br>" +
+                "<input value=\"Upload\" type=\"submit\"></form>" +
+                "Upload only text files.";
+
+        return responseString;
     }
 }
